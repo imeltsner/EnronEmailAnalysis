3c3a7458889541efacf74d573ad9b503a940be6c
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-05-09 17:34:44 -0700

    fixed parsing

--- Parser.java
+++ Parser.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -43,20 +42,56 @@ public class Parser {
     private void processFile(File file) throws FileNotFoundException {
         Scanner scan = new Scanner(file);
         String line = new String();
+        String sender = new String();
 
         while (scan.hasNextLine()) {
             line = scan.nextLine();
-            parseLine(line);
+            if (line.startsWith("From:")) {
+               sender = parseSender(line);
+            }
+            else if (line.startsWith("To:") || line.startsWith("\t")) {
+                parseRecipient(line, sender);
+            }
         }
         scan.close();
     }
 
-    private void parseLine(String line) {
+    private String parseSender(String line) {
         Matcher matcher = Pattern.compile("([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9._-]+)").matcher(line);
-        if (line.startsWith("From:")) {
-            if (matcher.find()) {
-                sent.put(matcher.group(), new ArrayList<String>());
+        String sender = new String();
+
+        if (matcher.find()) {
+            sender = matcher.group();
+            if (!sent.containsKey(sender)) {
+                sent.put(sender, new ArrayList<String>());
             }
         }
+        return sender;
+    }
+
+    private void parseRecipient(String line, String sender) {
+        Matcher matcher = Pattern.compile("([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9._-]+)").matcher(line);
+
+        while (matcher.find()) {
+            String recipient = matcher.group();
+            if (sent.get(sender) != null) {
+                sent.get(sender).add(recipient);
+                if (!received.containsKey(recipient)) {
+                    received.put(recipient, new ArrayList<String>());
+                    received.get(recipient).add(sender);
+                }
+                else {
+                    received.get(recipient).add(sender);
+                }
+            } 
+        }
+    }
+
+    public HashMap<String, ArrayList<String>> getSent() {
+        return sent;
+    }
+
+    public HashMap<String, ArrayList<String>> getReceived() {
+        return received;
     }
 }
